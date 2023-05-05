bfe215a1326c7ade9e653910d5e799ad33588452
DodiElektrodi <maks.wojciechowski@gmail.com>
2023-05-05 15:42:44 +0200

    Ok Działa model jazdy i podstawy animacji gracza. Zaczynam robić ekran początkowy i końcowy.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
 import math
+import random
 
 # załączam pygame
 pygame.init()
 # ustawiam wielkość ekranu
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((900, 600))
 # ustawiam nazwę i miniature
 pygame.display.set_caption("Blok Ekipa Drag Race Game")
 icon = pygame.image.load("sport-car.png")
@@ -12,87 +13,130 @@ pygame.display.set_icon(icon)
 
 # Gracz
 playerImg = pygame.image.load("enemy_car.jpeg")
+playerImg = pygame.transform.scale(playerImg, (500, 200))
 playerX = -200
 playerY = 100
-playerChange = 0
-gas_lvl = 1
-clutch_pressed = False
-gear = 1
-velocity = 0
-d_velocity = 0
-RPM = 3000
-mass = 1800
-power = 73550
+# Parametry fury
+gear_box = {0: float('inf'), 1: 27.5, 2: 13.7, 3: 9.1, 4: 7.4, 5: 5.3, 6: 4.37}
 
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 
-def gear_shift():
-    global gear
-    if not clutch_pressed or gear == 6:
+def gear_shift(num):
+    global gear, gear_shifted
+    if not clutch_pressed:
         print("Honk!!!")
     else:
-        print("Click")
-        gear += 1
+        gear_shifted = num - gear
+        gear = num
 
 
-def calc_drag():
-    cross_sectio = 1.77
-    drag_co = 0.3
-    air_density = 1.225
-    return 0.5 * air_density * velocity ** 2 * drag_co * cross_sectio
+def game_over():
+    print("Game over")
 
 
-def calc_dv():
-    x = RPM / 1000
-    power_out = x * math.sqrt(11 - x) * 0.0712134 * power
-    work = power_out * gas_lvl * dt
-    dwork = work - calc_drag() * dt
-    if dwork >= 0:
-        dv = math.sqrt((2 * dwork) / mass)
-    else:
-        dv = math.sqrt(((-2) * dwork) / mass) * (-1)
-    return dv
+def clutch_release():
+    global RPM, clutch_pressed, gear_shifted
+    if gear_shifted:
+        RPM -= 2500 * gear_shifted
+        gear_shifted = 0
+    clutch_pressed = False
 
 
-def speed_to_rpm():
-    global RPM
-    # stosunki przekładni
-    sheet = {1: 15.466, 2: 9.082, 3: 6.845, 4: 4.052, 5: 2.892, 6: 2.310}
-    shaft_rpm = velocity / (math.pi * 0.51)
-    RPM = shaft_rpm * sheet[gear]
-    if RPM > 8000: RPM = 8000
+def d_rpm():
+    amp = math.floor(abs(7250 - RPM) / 400)
+    return int((26 - amp) * math.sqrt(6 / gear))
 
 
-# tu śmiga pętla gry
+def rpm_to_speed():
+    shaft_rpm = RPM / gear_box[gear]
+    # 2 jest od 2 m obwodu koła
+    speed = shaft_rpm * 2 * 60 / 1000
+    return speed
+
+
+# Starting stats
+game_state = "EP"
+clock = pygame.time.Clock()
+gear = 0
+clutch_pressed = False
+velocity = 0
+RPM = 1000
+gear_shifted = 0
+distance = 0
+wait = True
 run = True
+tick = 0
+# Start
 while run:
-    clock = pygame.time.Clock()
-    dt = clock.tick(60) / 1000
+    dt = clock.tick(30) / 1000  # w sekundach
     screen.fill((0, 0, 0))
+    if tick == 10:
+        game_state = "GP"
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_1: gas_lvl = 1
-            if event.key == pygame.K_2: gas_lvl = 2
-            if event.key == pygame.K_3: gas_lvl = 3
-            if event.key == pygame.K_4: gas_lvl = 4
-            if event.key == pygame.K_5: gas_lvl = 5
-            if event.key == pygame.K_i: gear_shift()
-            if event.key == pygame.K_SPACE: clutch_pressed = True
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_SPACE: clutch_pressed = False
+# ___Ekran Początkowy
+    if game_state == "EP":
+        # pętla eventów
+        tick += 1
+        for event in pygame.event.get():
+            # to umożliwia wyjście
+            if event.type == pygame.QUIT:
+                run = False
 
-    if not clutch_pressed:
-        velocity += calc_dv()
-        speed_to_rpm()
+# ___Rozgrywka___
+    if game_state == "GP":
+        # pętla eventów
+        for event in pygame.event.get():
+            # to umożliwia wyjście
+            if event.type == pygame.QUIT:
+                run = False
+            # tu mamy całe sterowanie
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1: gear_shift(1)
+                if event.key == pygame.K_2: gear_shift(2)
+                if event.key == pygame.K_3: gear_shift(3)
+                if event.key == pygame.K_4: gear_shift(4)
+                if event.key == pygame.K_5: gear_shift(5)
+                if event.key == pygame.K_6: gear_shift(6)
+                if event.key == pygame.K_SPACE: clutch_pressed = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE: clutch_release()
 
-    print(gas_lvl, clutch_pressed, gear, RPM, velocity)
-    player()
+        # tu są wszystkie akcje w zależności od trzymania sprzęgła
+        if not clutch_pressed and gear != 0:
+            RPM += d_rpm()
+            velocity = rpm_to_speed()
+        if clutch_pressed or gear == 0:
+            RPM += 70
+            if velocity > 0: velocity -= 0.2
 
+        # tu jest zgrywanie ze sobą RPM, distance i velocity
+        if velocity < 0: velocity = 0
+        distance += velocity * dt / 3.6  # 3.6 daje nam metry na sekundę
+        if RPM > 8000:
+            RPM -= 85
+        if RPM < 1000:
+            game_over()
+            break
+
+        # tu mamy animowanie naszego pojazdu
+        # zacznijmy od ruchów góra dół
+        amp = 2 * (random.random() - 0.5)  # zwraca "losową" wartość [-1,1]
+        dplayerY = playerY + amp * ((RPM / 4000) * (velocity / 100))
+        # tu mamy wyjechanie na środek ekranu na start
+        if distance < 8:
+            playerX = distance * 50 - 200
+        else:
+            playerX = 200
+        player(playerX, dplayerY)
+        dplayerY = dplayerY
+
+        print(gear, clutch_pressed, RPM, velocity, distance)
+
+# ___Ekran Końcowy___
+    if game_state == "EK":
+        pass
     # ta linijka robi odświeżenie ekranu
     pygame.display.update()
